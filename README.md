6b5c548120e7cd445fb4859155b1286902e149bc
ainzs-evil-twin <epsilo@protonmail.com>
2020-10-21 00:15:17 +0530

    First commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# the-oracle
+
+Next challenge from Ainz's evil twin.
+
+## Description
+
+Evil twin was furious due to the last failure. This time he claimed that we need to peek into the future. Try getting the flag
+
+
+## Attachments
+
+`https://github.com/ainzs-evil-twin/the-oracle/blob/master/main.py`
+
+## Flag format
+
+`ictf{.+}`
+
+## Category
+
+Reversing, OSINT
+
+## Hints
+
+<details> 
+    <summary>Hint 1</summary> 
+    They forgot to `git merge`?
+</details>
---
+++ main.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+from codecs import decode
+from hashlib import md5
+from this import s as zen
+
+def xored_list(str1, str2):
+	return [ord(a) ^ ord(b) for a,b in zip(str1, str2)]
+
+flag = input("Paste flag here to get success message: ")
+
+zen = zen.split()
+
+# Since you outsmarted me in `see-the-past` no commit history this time
+index_str = <redacted>
+assert md5(index_str.encode()).hexdigest() == '4bd23eb13dd04f80b0459b5f2de6c8e7'
+
+x, y, z = index_str[0:2], index_str[2:4], index_str[4:6]
+key = zen[int(x)] + '_' + zen[int(y)] + '_' + zen[int(z)]
+key = decode(key, 'rot_13')
+
+xored = xored_list(flag, key)
+
+if xored == <Have not decided on it yet>:
+	print('\n\nSuccess :)')
+else:
+	print('\n\nFailure :(')
